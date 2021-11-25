88871033ff23f404cf38fd7e2f2bd63c7d3df163
Denis Molodtsov <denis-ne@yandex.ru>
2021-11-25 09:50:07 -0500

    catch nuget for ubuntu

--- PowerShell/AdoPipeline/Install-PowerShellModules.ps1
+++ PowerShell/AdoPipeline/Install-PowerShellModules.ps1
@@ -1,9 +1,13 @@
 Param (
     [Parameter()]
     [String]
-    $PnPVersion = "1.8.0" # TODO: We might want to get the latest version each time.
+    $PnPVersion = "1.8.0" # ! We might want to get the latest version each time.
 )
-Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser"
+try{
+    Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser"
+}catch{
+    Write-Host failed to install Nuget
+}
 if ($PnPVersion -ne $null -and $PnPVersion -ne "") {
     Install-Module PnP.PowerShell -Scope "CurrentUser" -RequiredVersion $PnPVersion -Verbose -AllowClobber -Force
 }
